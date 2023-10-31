f71d11ca0f041b4360fdf1c8fea14a8ce0d5c971
whatmakeart <79048796+whatmakeart@users.noreply.github.com>
2023-10-31 06:24:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,62 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let circles = [];
+let shapes = [];
 
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
 
-function createCircle(x, y) {
-    const numCircles = 10;
-    for (let i = 0; i < numCircles; i++) {
-        circles.push({
-            x: x,
-            y: y,
-            radius: i * 20,
-            lineWidth: (i * 20) * 0.05,
+function createShape(x, y) {
+    const points = [];
+    const numPoints = Math.floor(Math.random() * (75 - 6 + 1)) + 6;
+    const angleStep = Math.PI * 2 / numPoints;
+
+    for (let i = 0; i < numPoints; i++) {
+        points.push({
+            angle: i * angleStep,
+            distance: 20,
             color: `hsl(${Math.random() * 360}, 100%, 50%)`
         });
     }
+
+    shapes.push({ x, y, points });
 }
 
-function drawCircles() {
+function drawShapes() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    circles.forEach(circle => {
-        ctx.beginPath();
-        ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
-        ctx.strokeStyle = circle.color;
-        ctx.lineWidth = circle.lineWidth;
-        ctx.stroke();
-        ctx.closePath();
-        
-        circle.radius += 1;
-        circle.lineWidth = circle.radius * 0.05;
 
-        if (circle.radius > Math.sqrt(canvas.width**2 + canvas.height**2)) {
-            const index = circles.indexOf(circle);
-            circles.splice(index, 1);
+    shapes.forEach(shape => {
+        shape.points.forEach(point => {
+            ctx.beginPath();
+            const x = shape.x + Math.cos(point.angle) * point.distance;
+            const y = shape.y + Math.sin(point.angle) * point.distance;
+            ctx.arc(x, y, point.distance * 0.1, 0, Math.PI * 2);
+            ctx.fillStyle = point.color;
+            ctx.fill();
+            ctx.closePath();
+
+            point.distance += 1;
+        });
+
+        shape.points = shape.points.filter(point => {
+            return point.distance < Math.sqrt(canvas.width**2 + canvas.height**2);
+        });
+
+        if (!shape.points.length) {
+            shapes.shift();
         }
     });
 }
 
 function animate() {
     requestAnimationFrame(animate);
-    drawCircles();
+    drawShapes();
 }
 
 canvas.addEventListener('mousemove', (event) => {
-    createCircle(event.clientX, event.clientY);
+    createShape(event.clientX, event.clientY);
 });
 
 resizeCanvas();
